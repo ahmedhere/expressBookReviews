6014cc108c922e469be27e36ff0f949b120a325c
Muhammad Ahmed <techahmedd@gmail.com>
2026-08-19 06:59:46 -0400

    Resolved the issue

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,83 +1,118 @@
 const express = require('express');
+const axios = require('axios');
+
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
+
 const public_users = express.Router();
 
+public_users.post("/register", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
 
-public_users.post("/register", (req,res) => {
-        const username = req.body.username;
-        const password = req.body.password;
-    
-        // Check if both username and password are provided
-        if (username && password) {
-            // Check if the user does not already exist
-            if (!isValid(username)) {
-                // Add the new user to the users array
-                users.push({"username": username, "password": password});
-                return res.status(200).json({message: "User successfully registered. Now you can login"});
-            } else {
-                return res.status(404).json({message: "User already exists!"});
-            }
+    if (username && password) {
+        if (!isValid(username)) {
+            users.push({
+                username: username,
+                password: password
+            });
+
+            return res.status(200).json({
+                message: "User successfully registered. Now you can login"
+            });
+        } else {
+            return res.status(404).json({
+                message: "User already exists!"
+            });
         }
-        // Return error if username or password is missing
-        return res.status(404).json({message: "Unable to register user."});
+    }
+
+    return res.status(404).json({
+        message: "Unable to register user."
     });
-
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).send(JSON.stringify(books, null, 4));
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  const isbn = req.params.isbn;
 
-  const book = books[Number(isbn)];
-
-  return res.status(300).json(JSON.stringify(book, null, 4));
+// Task 10
+// Get the book list available in the shop using Axios + Promise
+public_users.get('/', function (req, res) {
+    axios.get('http://localhost:5000/')
+        .then(response => {
+            res.status(200).json(response.data);
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: "Unable to retrieve books"
+            });
+        });
 });
 
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-    //Write your code here
+
+// Task 11
+// Get book details based on ISBN using Axios + async/await
+public_users.get('/isbn/:isbn', async function (req, res) {
+    try {
+        const isbn = req.params.isbn;
+
+        const response = await axios.get(
+            `http://localhost:5000/isbn/${isbn}`
+        );
+
+        res.status(200).json(response.data);
+    } catch (error) {
+        res.status(500).json({
+            error: "Unable to retrieve book details"
+        });
+    }
+});
+
+
+// Task 12
+// Get book details based on author using Axios + Promise
+public_users.get('/author/:author', function (req, res) {
     const author = req.params.author;
-    let book = {}
-    for(const [key, value] of Object.entries(books)){
-        if(value.author === author){
-            book = value;
-            break;
-        }
-    }
-    
-    return res.status(300).json(JSON.stringify(book, null, 4));
+
+    axios.get(
+        `http://localhost:5000/author/${encodeURIComponent(author)}`
+    )
+        .then(response => {
+            res.status(200).json(response.data);
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: "Unable to retrieve books by author"
+            });
+        });
 });
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  const title = req.params.title;
-    let book = {}
-    for(const [key, value] of Object.entries(books)){
-        if(value.title === title){
-            book = value;
-            break;
-        }
+
+// Task 13
+// Get book details based on title using Axios + async/await
+public_users.get('/title/:title', async function (req, res) {
+    try {
+        const title = req.params.title;
+
+        const response = await axios.get(
+            `http://localhost:5000/title/${encodeURIComponent(title)}`
+        );
+
+        res.status(200).json(response.data);
+    } catch (error) {
+        res.status(500).json({
+            error: "Unable to retrieve books by title"
+        });
     }
-    
-    return res.status(300).json(JSON.stringify(book, null, 4));
 });
 
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  const isbn = req.params.isbn;
 
-  const book = books[Number(isbn)];
+// Get book review
+public_users.get('/review/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
 
-  return res.status(300).json(JSON.stringify(book.reviews, null, 4));
+    const book = books[Number(isbn)];
+
+    return res.status(200).json(book ? book.reviews : {});
 });
 
 module.exports.general = public_users;
